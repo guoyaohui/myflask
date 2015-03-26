2289e722a1d255bb3eefecc4b52479862b1744f4
guoyaohui <gyh657614602@126.com>
2015-03-26 22:25:52 +0800

    <1ag>

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def index():
      return '<h1>Hello World!\nHello Liuying<h1>'
 
 if __name__ == '__main__':
-     app.run(debug == True)
+     app.run(debug = True)
