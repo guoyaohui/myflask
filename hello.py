d5c620e28515350ef06ea858accfe67fe3789848
guoyaohui <gyh657614602@126.com>
2015-03-26 22:41:43 +0800

    change route

--- hello.py
+++ hello.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def index():
      return '<h1>Hello World!\nHello Liuying<h1>'
 
-@app.route('/user/name')
+@app.route('/user/<name>')
 def user(name):
-     return '<h2>Hello Spring !<h2>' % name
+     return '<h2>Hello Spring %s !<h2>' % name
 
 if __name__ == '__main__':
      app.run(debug = True)
